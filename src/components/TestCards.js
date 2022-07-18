ab3e1e6cf2c1f8a3b0886b8700c3d530074707e6
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-18 22:52:16 +0530

    bug fixed || layout good

--- src/components/TestCards.js
+++ src/components/TestCards.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TestCards({transform, name, designation}) {
   return (
-    <div className={`lg:flex-[0.3_0_0%] rounded my-5 mx-10 lg:mx-0 bg-neutral-800 hover:scale-[1.02] transition ${transform ? 'lg:h-80 h-64' : 'h-64'} text-white px-8 py-4 flex flex-col justify-between`}>
+    <div className={`lg:flex-[0.3_0_0%] rounded my-5 mx-10 lg:mx-0 bg-neutral-800 hover:scale-[1.02] transition ${transform ? 'h-auto lg:h-80 md:h-64' : 'h-auto md:h-64'} text-white px-8 py-4 flex flex-col justify-between`}>
         <font className = 'text-md font-light'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, harum aspernatur quibusdam facere magni adipisci eum, minima ipsa, doloremque reprehenderit laudantium nam. Reiciendis maxime esse accusantium. Harum aliquam ex quod?
         </font>
