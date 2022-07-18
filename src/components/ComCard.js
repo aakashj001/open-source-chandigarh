c87d78b6ab53a960c61d0c36c84ad458a13a7e55
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-18 14:46:53 +0530

    Making the cards || bugs found fix in progress

--- src/components/ComCard.js
+++ src/components/ComCard.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function ComCard() {
+function ComCard({title, number}) {
   return (
-    <div>ComCard</div>
+    <div className='flex-1 my-5 md:flex-[0.3_0_0%] h-56 rounded-xl bg-white shadow-lg hover:shadow-xl transition flex flex-col justify-center items-center'>
+        <font className = 'font-light text-4xl text-gray-500'>{number}</font>
+        <font className = 'font-light text-xl'>{title}</font>
+    </div>
   )
 }
 
--- src/components/Community.js
+++ src/components/Community.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import ComCard from './ComCard'
 
 function Community() {
   return (
     <div className='container m-auto flex flex-col mt-10 items-center justify-center'>
-        <div className='font-light text-3xl text-center'>
-            Our Community +
+        <div className='font-light text-3xl text-center border-b-2 pb-2 px-4 border-b-black'>
+            Our Community
+        </div>
+        <div className = "flex w-full h-auto justify-between flex-col md:flex-row mt-10">
+            <ComCard title={'Projects'} number = {'100+'}/>
+            <ComCard title={"Collaborators"} number = {'100+'}/>
+            <ComCard title={"Workshops"} number = {'50+'}/>
         </div>
     </div>
   )
