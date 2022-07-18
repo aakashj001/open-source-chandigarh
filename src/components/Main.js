62a60550345e79fa9f6bf4d4a92a8c106f653054
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-18 14:26:30 +0530

    Main Section completed || bits left

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function Main() {
   return (
-    <div className='container m-auto flex flex-col justify-center items-center'>
-        <div className='font-medium text-4xl mt-5'>
-            Open Source Chandigarh Powered by <font className = 'text-red-500 font-bold'>Chitkara University</font>
-        </div>
+    <div className="container m-auto flex flex-col justify-center items-center mt-10">
+      <div className="font-medium text-4xl text-center">
+        Open Source Chandigarh Powered by{" "}
+        <font className="text-red-500 font-semibold">Chitkara University</font>
+      </div>
+      <div className="font-light text-xl text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, corrupti tempora! Nobis atque voluptatum magni exercitationem? Aspernatur, excepturi labore rerum aliquid, iusto expedita maxime fuga qui ea autem possimus perspiciatis.
+      </div>
+      <div className="flex flex-row w-full mt-5 items-center justify-center">
+        <button className="bg-black rounded-xl m-5 px-4 py-2 font-medium text-white text-xl">
+          Check-Out Projects
+        </button>
+        <button className="bg-black rounded-xl m-5 px-4 py-2 font-medium text-white text-xl">
+          Learn More
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="sticky top-0 bg-white flex w-screen items-center h-14 shadow-2xl">
-      <div className="flex flex-[0.4_0_0%] font-medium text-xl ml-4">
+      <div className="flex flex-[0.4_0_0%] font-medium text-2xl ml-4">
         OpenSouce Chandigarh
       </div>
-      <div className="flex flex-[0.2_0_0%] flex-row items-center justify-between">
+      <div className="flex flex-[0.2_0_0%] flex-row items-center text-lg justify-between">
         <Link to={"/"}>Home</Link>
         <Link to={"/"}>Events</Link>
         <Link to={"/"}>Projects</Link>
         <Link to={"/"}>Team</Link>
       </div>
       <div className="flex flex-[0.4_0_0%] justify-end">
-        <button className="bg-gray-800 rounded-xl px-4 py-2 font-bold text-white text-xl mr-4">
+        <button className="bg-gray-800 rounded-xl px-4 py-2 font-semibold text-white text-xl mr-4">
           Start Contributing
         </button>
       </div>
