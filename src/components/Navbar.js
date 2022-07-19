b6241d83e2687fe80f74fd18f28340aa624679c0
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-19 11:52:19 +0530

    Responsive Navbar added || Background color bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,109 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { act } from "react-dom/test-utils";
+
 function Navbar() {
+  const [show, setShow] = useState(false);
   const loc = useLocation();
   useEffect(() => {
     let str = loc.pathname;
-    let currLoc = str.split("/")[1]
+    let currLoc = str.split("/")[1];
 
-    document.title = currLoc === '' ? 'OpenSource Chandigarh' : currLoc.charAt(0).toUpperCase() + currLoc.slice(1) + ' | OpenChd'
+    document.title =
+      currLoc === ""
+        ? "OpenSource Chandigarh"
+        : currLoc.charAt(0).toUpperCase() + currLoc.slice(1) + " | OpenChd";
 
-    const but = document.querySelectorAll(`a[href = '${str}']`)
-    const acLink = document.querySelectorAll('a')
-    acLink.forEach(active => {
-      active.classList.remove('bg-[rgba(0,0,0,0.15)]')
-    })
-    but.forEach(buts => {
-      if(!(buts.classList.contains('link'))){
-        buts.classList.add('bg-[rgba(0,0,0,0.15)]')
+    const but = document.querySelectorAll(`a[href = '${str}']`);
+    const acLink = document.querySelectorAll("a");
+    acLink.forEach((active) => {
+      if (active.classList.contains("light")) {
+        active.classList.remove("bg-[rgba(255,255,255,0.15)]");
+      } else {
+        active.classList.remove("bg-[rgba(0,0,0,0.15)]");
       }
-    })
-  }, [loc])
+    });
+    but.forEach((buts) => {
+      if (!buts.classList.contains("link")) {
+        if (buts.classList.contains("light")) {
+          buts.classList.add("bg-[rgba(255,255,255,0.15)]");
+        } else {
+          buts.classList.add("bg-[rgba(0,0,0,0.15)]");
+        }
+      }
+    });
+  }, [loc]);
   return (
     <div className="sticky top-0 z-[99999] bg-white md:bg-transparent md:backdrop-blur-md flex w-screen items-center h-16 shadow-2xl">
-      <div className="flex flex-1 md:flex-[0.4_0_0%] font-medium text-2xl ml-4">
+      <Link className="flex flex-1 md:flex-[0.4_0_0%] font-medium text-2xl ml-4 link" to={'/'}>
         OpenSource Chandigarh
-      </div>
+      </Link>
       <div className="hidden lg:flex flex-[0.3_0_0%] flex-row items-center text-lg justify-between">
-        <Link to={"/"} className = "px-4 py-2 rounded-full transition-all">Home</Link>
-        <Link to={"/events"} className = "px-4 py-2 rounded-full transition-all">Events</Link>
-        <Link to={"/projects"} className = "px-4 py-2 rounded-full transition-all">Projects</Link>
-        <Link to={"/team"} className = "px-4 py-2 rounded-full transition-all">Team</Link>
+        <Link to={"/"} className="px-4 py-2 rounded-full transition-all">
+          Home
+        </Link>
+        <Link to={"/events"} className="px-4 py-2 rounded-full transition-all">
+          Events
+        </Link>
+        <Link
+          to={"/projects"}
+          className="px-4 py-2 rounded-full transition-all"
+        >
+          Projects
+        </Link>
+        <Link to={"/team"} className="px-4 py-2 rounded-full transition-all">
+          Team
+        </Link>
       </div>
       <div className="hidden lg:flex flex-[0.4_0_0%] justify-end">
         <button className="bg-gray-800 rounded-xl px-4 py-2 font-semibold text-white text-xl mr-4">
           Start Contributing
         </button>
+      </div>
+
+      <div
+        className="flex-[0.1_0_0%] md:hidden justify-end "
+        onClick={() => setShow(!show)}
+      >
+        <AiOutlineMenu size={25} />
+      </div>
+      <div
+        className={`top-[3.5rem] ${
+          show ? "flex" : "hidden"
+        } absolute left-0 right-0 z-50 space-y-5 flex md:hidden flex-col text-center transition-all shadow-2xl bg-neutral-800 p-8 text-white`}
+      >
+        {/* <AiOutlineArrowLeft onClick={() => setShow(!show)} size={20} className="fixed top-2 left-2"/> */}
+        <Link
+          to={"/"}
+          onClick={() => setShow(!show)}
+          className="py-2 rounded-xl light"
+        >
+          Home
+        </Link>
+        <Link
+          to={"/events"}
+          onClick={() => setShow(!show)}
+          className="py-2 rounded-xl light"
+        >
+          Events
+        </Link>
+        <Link
+          to={"/projects"}
+          onClick={() => setShow(!show)}
+          className="py-2 rounded-xl light"
+        >
+          Projects
+        </Link>
+        <Link
+          to={"/team"}
+          onClick={() => setShow(!show)}
+          className="py-2 rounded-xl light"
+        >
+          Team
+        </Link>
       </div>
     </div>
   );
