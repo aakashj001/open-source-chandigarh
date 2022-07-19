b5f04b38b551a338a5d72633f96de6d7b625180b
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-19 21:21:23 +0530

    Content Added in Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,30 @@
 import React from "react";
-import heroImg from '../images/work-ic.png'
+import { Link } from "react-router-dom";
+import heroImg from "../images/work-ic.png";
 
 function Main() {
   return (
     <div className="container m-auto flex flex-col justify-center items-center mt-2">
-      <img src={heroImg} alt="Hero Banner" className="w-full md:w-[70rem]"/>
+      <img src={heroImg} alt="Hero Banner" className="w-full md:w-[60rem]" />
       <div className="font-medium text-2xl md:text-4xl text-center w-full mx-10">
         Open Source Chandigarh Powered by{" "}
         <font className="text-red-500 font-semibold">Chitkara University</font>
       </div>
-      <div className="font-extralight text-sm mx-8 mt-4 md:text-xl text-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, corrupti tempora! Nobis atque voluptatum magni exercitationem? Aspernatur, excepturi labore rerum aliquid, iusto expedita maxime fuga qui ea autem possimus perspiciatis.
+      <div className="font-extralight text-sm mx-8 mt-10 flex flex-col items-center justify-center md:text-xl text-center">
+        <font className="border-l border-r border-black px-4">
+          To present the benefits of network effects of open-source communities
+          to budding engineers and existing technicians in the Tri-city area
+        </font>
+        <br />
+        <font className = "border-l border-r border-black px-4">
+          To build a robust and dignified ecosystem for the advancement of
+          open-source technologies and promote the same in the Tri-City
+        </font>
       </div>
       <div className="flex flex-col md:flex-row w-full mt-5 my-10 items-center justify-center">
-        <button className="bg-black rounded-xl mx-5 my-2 px-4 py-2 font-medium text-white text-md md:text-xl">
+        <Link to={"/projects"} className="bg-black rounded-xl mx-5 my-2 px-4 py-2 font-medium text-white text-md md:text-xl">
           Check-Out Projects
-        </button>
+        </Link>
         <button className="bg-black rounded-xl mx-5 my-2 px-4 py-2 font-medium text-white text-md md:text-xl">
           Learn More
         </button>
