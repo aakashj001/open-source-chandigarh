de6caea34c5772b8c78830287a42f7c677cb2c36
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-19 11:15:47 +0530

    basic bug fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import ProjCards from './ProjCards'
 
 function Projects() {
   return (
-    <div className='container m-auto my-10 px-16 py-8 shadow-2xl flex flex-row flex-wrap justify-around'>
+    <div className='container m-auto md:my-10 px-16 py-8 shadow-2xl flex flex-row flex-wrap justify-around'>
         <ProjCards />
         <ProjCards />
         <ProjCards />
