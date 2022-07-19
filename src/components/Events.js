b4a6d30793a3621fa24a157cd149657e3d4f4c59
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-19 22:16:27 +0530

    Content Added || project page addition || Events made

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import EventCard from "./EventCard";
 
 function Events() {
   return (
-    <div className='container m-auto md:mt-10 flex flex-col justify-center px-4 md:px-24 py-12'>
-        <font className = 'text-2xl md:text-5xl md:px-2 pb-2 md:pb-4 border-b border-b-black'>Upcoming Events</font>
+    <div className="container m-auto flex flex-col justify-center shadow-2xl my-5 px-4 md:px-24 py-12">
+      <font className="text-3xl md:text-5xl md:px-2 pb-2 md:pb-4 border-b border-b-black">
+        Upcoming Events
+      </font>
+      <div className="mt-10 flex flex-row flex-wrap items-center justify-around">
+        <EventCard date={"July 20, 2022"} title={"Inaugral"} />
+        <EventCard date={"August 1, 2022"} title={"Open-Source Drive-I"} />
+        <EventCard date={"September 5, 2022"} title={"Open-Source Drive-II"} />
+        <EventCard date={"September 23, 2022"} title={"Hackathon 48 Hrs"} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Events
+export default Events;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import ProjCards from './ProjCards'
+import React from "react";
+import ProjCards from "./ProjCards";
 
 function Projects() {
   return (
-    <div className='container m-auto md:my-10 px-8 md:px-16 py-8 shadow-2xl flex flex-row flex-wrap justify-around'>
+    <div className="container m-auto md:my-10 px-8 md:px-16 py-8 shadow-2xl flex flex-col justify-around">
+      <font className="text-3xl md:text-5xl md:px-2 pb-2 md:pb-4 border-b border-b-black">
+        Open-Source Projects
+      </font>
+      <div className="flex flex-row flex-wrap w-full justify-around mt-10">
         <ProjCards />
         <ProjCards />
         <ProjCards />
+        <ProjCards />
+        <ProjCards />
+        <ProjCards />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
