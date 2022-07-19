230b5756f8a3be108f28ad7154f0a475712f2f58
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-19 21:44:09 +0530

    Most of the content added || testimonials removed for now

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
               <Main />
               <Community />
               <Core />
-              <Testimonials />
+              {/* <Testimonials /> */}
             </>
           }
         />
