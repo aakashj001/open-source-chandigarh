43c94262e4ec4e80bc4a8f954de30738485e183a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-19 11:14:17 +0530

    projects page made

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ProjCards from './ProjCards'
 
 function Projects() {
   return (
-    <div className='container m-auto my-10 px-16 py-8 shadow-2xl flex flex-row flex-wrap'>
-        
+    <div className='container m-auto my-10 px-16 py-8 shadow-2xl flex flex-row flex-wrap justify-around'>
+        <ProjCards />
+        <ProjCards />
+        <ProjCards />
     </div>
   )
 }
