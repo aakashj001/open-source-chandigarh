609f2dee70fc751deafc5108a83b7e9b3fcf7719
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-29 22:16:50 +0530

    Modal Window added for Large Screens Working on mobile view

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import {CgClose} from 'react-icons/cg'
 
-function EventCard({date, title, organiser, link}) {
+function EventCard({ date, title, organiser, link }) {
+  const [show, setShow] = useState(false);
   return (
-    <div className='w-[27em] h-64 my-4 shadow-md bpx-border px-4 py-2 flex flex-col justify-around hover:shadow-xl transition-all'>
-        <font className = 'font-bold text-4xl pb-2 border-b-2'>
-            {date ? date : "Date here"}
+    <>
+      <div className="w-[27em] h-64 my-4 shadow-md bpx-border px-4 py-2 flex flex-col justify-around hover:shadow-xl transition-all" onClick={() => setShow(!show)}>
+        <font className="font-bold text-4xl pb-2 border-b-2">
+          {date ? date : "Date here"}
         </font>
-        <font className = 'font-light text-3xl'>
-            {title ? title : "Title here"}
+        <font className="font-light text-3xl">
+          {title ? title : "Title here"}
         </font>
-        <font className = 'font-normal text-xl mt-3 self-end underline underline-offset-4'>
-            By: {organiser ? <a href={link} target = 'blank' className='text-red-600'>{' ' + organiser}</a> : "Organisers Here"}
+        <font className="font-normal text-xl mt-3 self-end underline underline-offset-4">
+          By:{" "}
+          {organiser ? (
+            <a href={link} target="blank" className="text-red-600">
+              {" " + organiser}
+            </a>
+          ) : (
+            "Organisers Here"
+          )}
         </font>
-    </div>
-  )
+      </div>
+      {show && (
+        <div className={`fixed w-screen h-screen top-16 backdrop-blur-sm flex flex-col items-center justify-center bg-[rgba(0,0,0,0.2)]`}>
+          <div className="w-3/4 h-[60vh] bg-white rounded-xl relative top-[-4rem] flex flex-col justify-around items-center py-8 px-4">
+            <button className="absolute top-8 right-4" onClick={() => setShow(!show)}> <CgClose size={45}/></button>
+            <h1 className="text-5xl font-bold underline underline-offset-4">
+              {title}
+            </h1>
+            <font className = "self-end text-2xl text-black text-right">
+              By: {<font className="text-red-500">{organiser}</font>}
+              <br />
+              <font className = 'text-lg'>To know more <a href={link} className = 'text-blue-500 underline underline-offset-4' target="blank"> Click Here </a></font>
+            </font>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default EventCard
+export default EventCard;
