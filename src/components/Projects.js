9809508b3f3d8bafa330f3103e9845b3bc96a0d7
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-07-29 16:02:59 +0530

    Removed redundant code || dummy cards removed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ProjCards from "./ProjCards";
+// import ProjCards from "./ProjCards";
 
 function Projects() {
   return (
@@ -8,12 +8,12 @@ function Projects() {
         Open-Source Projects
       </font>
       <div className="flex flex-row flex-wrap w-full justify-around mt-10">
+        {/* <ProjCards />
         <ProjCards />
         <ProjCards />
         <ProjCards />
         <ProjCards />
-        <ProjCards />
-        <ProjCards />
+        <ProjCards /> */}
       </div>
     </div>
   );
